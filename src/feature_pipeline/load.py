0ea163a867f0995860604ddd57c39e74d9cf434c
baniasbaabe <72874670+baniasbaabe@users.noreply.github.com>
2023-10-18 13:33:28 +0200

    Refactor utility functions for login and loading features

--- src/feature_pipeline/load.py
+++ src/feature_pipeline/load.py
@@ -8,28 +8,21 @@ from hsfs.feature_store import FeatureStore
 
 from hsfs.feature_group import FeatureGroup
 
+import utils
+
 
 class Loader:
     def __get_project(self) -> Project:
         logger.info("Getting project...")
-        return hopsworks.login(
-            api_key_value=os.getenv("HOPSWORKS_API_KEY"),
-            project=os.getenv("HOPSWORKS_PROJECT_NAME"),
-        )
+        return utils.hopsworks_login()
 
     def __get_feature_store(self, project: Project) -> FeatureStore:
         logger.info("Getting feature store...")
-        return project.get_feature_store()
+        return utils.hopsworks_get_feature_store(project)
 
     def __get_feature_group(self, fs: FeatureStore) -> FeatureGroup:
         logger.info("Getting feature group...")
-        return fs.get_or_create_feature_group(
-            name="air_quality_timeseries",
-            version=1,
-            description="Timeseries for every sensor ID",
-            event_time="dt",
-            primary_key=["SID"],
-        )
+        return hopsworks_get_feature_group(fs)
 
     def __insert_data(self, fg, df: pl.DataFrame) -> FeatureGroup:
         logger.info("Inserting data into feature store...")
---
+++ src/utils.py
@@ -0,0 +1,26 @@
+import hopsworks
+import os
+from hopsworks.project import Project
+from hsfs.feature_store import FeatureStore
+from hsfs.feature_group import FeatureGroup
+
+
+def hopsworks_login():
+    return hopsworks.login(
+        api_key_value=os.getenv("HOPSWORKS_API_KEY"),
+        project=os.getenv("HOPSWORKS_PROJECT_NAME"),
+    )
+
+
+def hopsworks_get_feature_store(project: Project) -> FeatureStore:
+    return project.get_feature_store()
+
+
+def hopsworks_get_feature_group(fs: FeatureStore) -> FeatureGroup:
+    return fs.get_or_create_feature_group(
+        name="air_quality_timeseries",
+        version=1,
+        description="Timeseries for every sensor ID",
+        event_time="dt",
+        primary_key=["SID"],
+    )
